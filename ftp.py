1dbf040f1217c935f47d74c2e49e8f5a51ba27c7
SoA-Lee <llsa0927@naver.com>
2021-08-02 17:58:32 +0900

    main

--- ftp.py
+++ ftp.py
@@ -1,5 +1,9 @@
 # 실행시킨 폴더 안에 설치됨.
 
 import subprocess
+import os
 
 subprocess.call('git clone https://github.com/nikdubois/vsftpd-2.3.4-infected.git', shell = True)
+os.system('sudo apt-get install build-essential')
+os.system('cp -f /vsftpd-2.3.4-infected/Makefile Makefile2.txt')
+os.system('cd vsftpd-2.3.4-infected')
