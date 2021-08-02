64000941348b2abe2a7af4f59b664db9529c04b3
SoA-Lee <llsa0927@naver.com>
2021-08-02 18:31:23 +0900

    edit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import subprocess
 import os
 
+os.system('cd jupyternootebook')
 subprocess.call('git clone https://github.com/nikdubois/vsftpd-2.3.4-infected.git', shell = True)
 os.system('sudo apt-get install build-essential')
 os.system('cp -f /vsftpd-2.3.4-infected/Makefile Makefile2.txt')
